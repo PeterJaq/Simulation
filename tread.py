c19c0d5a8a97da28400e14563466be30dcf1c101
peterjaq <anqing_jiang@163.com>
2019-01-03 12:45:10 +0800

    --two tread, Main tread and simulation tread. work

--- tread.py
+++ tread.py
@@ -8,25 +8,35 @@ import time
 #继承 QThread 类
 class BigWorkThread(QThread):
 
-    finishSignal = pyqtSignal(list)
-    def __init__(self, t,parent=None):
+    finishSignal = pyqtSignal(str)
+    updateSignal = pyqtSignal(list)
+
+    def __init__(self, workDistance, k, radian, wavelength,parent=None):
         super(BigWorkThread, self).__init__(parent)
-        self.t = t
+        self.workDistance = workDistance
+        self.k = k
+        self.wavelength = wavelength
+        self.radian = radian
 
 
     def run(self):
+
         if self.k == 0.0 or self.radian == 0.0:
             QMessageBox.information(self, "warning", "Please set the value of Radian or K")
         else:
-            simTest = sim(k=self.k, radian=self.radian, wavelength=632.8)
+            while self.workDistance <= 10:
+                simTest = sim(k=self.k, radian=self.radian, wavelength=632.8)
 
-            startPoint = int(self.workDistance * 10000)
-            self.I0, self.I = simTest.simluation(startPoint)
-            if self.workDistance > 10:
-                self.workDistance = 0
-            else:
-                self.workDistance += 0.1
-                time.sleep(1)
-                self.auto.emit(1)
+                startPoint = int(self.workDistance * 10000)
+                self.I0, self.I = simTest.simluation(startPoint)
+                if self.workDistance > 10:
+                    self.workDistance = 0
+                else:
+                    self.workDistance += 0.1
+                    time.sleep(1)
+                    self.updateSignal.emit([self.I0, self.I, self.workDistance])
 
-        self.finishSignal.emit(['tread Finish'])
+            self.finishSignal.emit("Finish")
+            self.workDistance = 0
+
+
